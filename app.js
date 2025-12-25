84c0ff579391953a0715fec9cd0ad4d7db92a7c5
Mauthope <mauricioprestesgrigol@gmail.com>
2025-12-25 09:33:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const VIDEO_URL = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+const VIDEO_URL = "https://studio.youtube.com/video/oDFEabgnR_c/edit";
 const WHATSAPP_NUMBER = "5549988971962";
 const WHATSAPP_MESSAGE =
   "Olá! Eu confirmei meu e-mail e quero continuar minha presença digital.";
